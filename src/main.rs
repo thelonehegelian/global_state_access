04d2b6bd953c0d436ff37bbe4de67b205ee38dff
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-07 23:20:21 +0000

    Add explanation on threading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+/**
+ * Explanantion for why Mango is added last though it appears first in the code:
+ * The order in which the items are added to the vector is not
+ *  guaranteed because there are multiple threads modifying the vector concurrently.
+ * In this case, the main thread and the thread spawned on line 29 both try to acquire a
+ * lock on the Mutex and modify the vector. The order in which they acquire the lock
+ * and modify the vector is not deterministic and can vary between runs.
+ * This is what leads to the seemingly random order in which the items are added to the vector.
+ */
 use lazy_static::lazy_static;
 use std::sync::Mutex;
 use std::thread;
